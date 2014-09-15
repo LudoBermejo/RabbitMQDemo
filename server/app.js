67c9a3988bfacfcc234d133ee6c4dff579aff4ae
Ludo Bermejo <ludo.bermejo@itoptraining.com>
2014-09-15 12:33:28 +0200

    Jobs and results queue

--- server/app.js
+++ server/app.js
@@ -1,36 +1,12 @@
-var express = require('express');
-
 /******************************************
  *
  *  App
  *
  ******************************************/
 
-var app = express();
 
 var RabbitMQModel = require('./models/rabbitMQ');
 
-
-
-
-var port;
-// development error handler
-// will print stacktrace
-
-if (app.get('env') === 'development') {
-    port = 8095;
-}
-else if (app.get('env') === 'test') {
-    port = 8095;
-}
-// production error handler
-// no stacktraces leaked to user
-else
-{
-    port = 8090;
-}
-
-port = Math.round(Math.random()*10000);
 
 /******************************************
  *
@@ -38,13 +14,9 @@ port = Math.round(Math.random()*10000);
  *
  ******************************************/
 
-app.listen(port, function() {
-    console.log('Express server listening on port ' + port);
-});
-
-
 RabbitMQModel.init().then(function()
 {
+    RabbitMQModel.prepareResultConsumer();
     for(var i=0;i<=10;i++)
     {
         var obj = { id: i, name:"Job server port " + port + " #" + i}
--- server/models/rabbitMQ.js
+++ server/models/rabbitMQ.js
@@ -10,13 +10,31 @@ rabbitMQ.init = function()
 {
     return new Promise(function (resolve, reject) {
 
-        amqp.connect('amqp://192.168.1.135').then(function (conn) {
+        amqp.connect('amqp://54.77.209.64').then(function (conn) {
+            console.log("A")
             conn.createConfirmChannel().then(function (ch) {
                 channel = ch;
                 resolve();
             });
         });
     });
+}
+
+rabbitMQ.prepareResultConsumer = function(obj)
+{
+    var q = "result_queue";
+    var ok = channel.assertQueue(q, {durable: true});
+    ok = ok.then(function() { ch.prefetch(1); });
+    ok = ok.then(function() {
+        ch.consume(q, doWork, {noAck: false});
+        console.log(" [*] Waiting for messages. To exit press CTRL+C");
+    });
+}
+
+rabbitMQ.onResult= function(msg) {
+    console.log(" [x] Received '%s'", body);
+    channel.ack(msg);
+
 }
 
 rabbitMQ.sendJob = function(obj)
