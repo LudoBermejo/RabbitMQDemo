d6ff9a71d39f1e8445820a4d830e4cf5c291e7d2
Ludo Bermejo <ludo.bermejo@itoptraining.com>
2014-09-16 15:38:59 +0200

    Java by node-child

--- client/models/rabbitMQ.js
+++ client/models/rabbitMQ.js
@@ -1,6 +1,6 @@
 var amqp = require('amqplib');
 
-var rabbitMQServer = require('../conf/RabbitMQ.json');
+var configuration = require('../conf/config.json');
 
 var amqp = require('amqplib');
 var Promise = require('bluebird');
@@ -8,11 +8,12 @@ var Promise = require('bluebird');
 var rabbitMQ = {};
 var channel = null;
 
-rabbitMQ.init = function()
-{
+var totalJobsRunning = 0;
+
+rabbitMQ.init = function () {
     return new Promise(function (resolve, reject) {
 
-        amqp.connect(rabbitMQServer[process.env.NODE_ENV].host).then(function (conn) {
+        amqp.connect(configuration.RabbitMQ[process.env.NODE_ENV].host).then(function (conn) {
             conn.createConfirmChannel().then(function (ch) {
                 channel = ch;
                 resolve();
@@ -21,55 +22,50 @@ rabbitMQ.init = function()
     });
 }
 
-rabbitMQ.prepareJobsConsumer = function(obj)
-{
+rabbitMQ.prepareJobsConsumer = function (obj) {
     var q = "jobs_queue";
     var ok = channel.assertQueue(q, {durable: true});
-    ok = ok.then(function() { channel.prefetch(1); });
-    ok = ok.then(function() {
+    ok = ok.then(function () {
+        channel.prefetch(1);
+    });
+    ok = ok.then(function () {
         channel.consume(q, rabbitMQ.onJob, {noAck: false});
         console.log(" [*] Waiting for messages. To exit press CTRL+C");
     });
 }
-String.prototype.rtrim = function() {
-    return this.replace(/\s+$/,"");
+String.prototype.rtrim = function () {
+    return this.replace(/\s+$/, "");
 }
-rabbitMQ.onJob = function(msg)
+
+rabbitMQ.executeJAR = function(msg, mustACK)
 {
     var body = msg.content.toString();
-    console.log(" [x] Received '%s'", body);
-
-
-
     var exec = require('child_process').exec, child;
-    child = exec('java -jar java.jar', {
-        cwd: "java"
+
+    child = exec('java -jar ' + configuration.Java.file, {
+            cwd: configuration.Java.path
         },
-        function (error, stdout, stderr){
+        function (error, stdout, stderr) {
 
             var resp = { orig: JSON.parse(body), response: null}
-            if(error !== null){
+            if (error !== null) {
                 resp.response = { type: "error", value: err};
             }
-            if(stderr)
-            {
+            if (stderr) {
                 resp.response = { type: "error", value: stderr};
             }
-            else if(stdout)
-            {
+            else if (stdout) {
                 var cad = String(stdout.rtrim());
-                cad = cad.substr(cad.indexOf("{"),10000000)
-                cad = cad.substr(0, cad.indexOf("}")+1)
+                cad = cad.substr(cad.indexOf("{"), 10000000)
+                cad = cad.substr(0, cad.indexOf("}") + 1)
 
 
                 var file = JSON.parse(cad);
 
-                if(file == undefined || file == null)
-                {
+                if (file == undefined || file == null) {
                     resp.response = { type: "error", value: "No file returned"};
                 }
-                else
-                {
+                else {
                     console.log(file.file)
                     var result = require("../java/" + file.file);
                     resp.response = { type: "complete", value: result};
@@ -77,26 +73,38 @@ rabbitMQ.onJob = function(msg)
 
             }
 
-            channel.ack(msg);
+            if(mustACK) channel.ack(msg);
             rabbitMQ.sendResult(JSON.stringify(resp));
+            totalJobsRunning--;
         });
 
+}
+rabbitMQ.onJob = function (msg) {
+    var body = msg.content.toString();
+    console.log(" [x] Received '%s'", body);
 
-/*    setTimeout(function() {
-        console.log(" [x] Done and sending " + body);
+    var mustACK = false;
+    totalJobsRunning++;
+    if(totalJobsRunning < configuration.Java.totalInstances)
+    {
         channel.ack(msg);
-        rabbitMQ.sendResult(body);
-    }, 1000);*/
+    }
+    else
+    {
+        mustACK = true;
+    }
+
+    setTimeout(rabbitMQ.executeJAR,5000,msg, mustACK)
+
 
 }
 
-rabbitMQ.sendResult = function(obj)
-{
+rabbitMQ.sendResult = function (obj) {
     var q = 'result_queue';
     var ok = channel.assertQueue(q, {durable: true});
 
     var msg = JSON.stringify(obj);
-    ok.then(function() {
+    ok.then(function () {
         channel.sendToQueue(q, new Buffer(msg), {persistent: true}, function (err, ok) {
 
             if (err !== null)
